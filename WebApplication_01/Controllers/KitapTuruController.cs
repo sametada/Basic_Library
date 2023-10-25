82ba651eb628b04882f225030088747952a79e38
Samet <sametbas2010@hotmail.com>
2023-10-26 01:52:11 +0300

    mvc - aşama 1

--- WebApplication_01/Controllers/KitapTuruController.cs
+++ WebApplication_01/Controllers/KitapTuruController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication_01.Models;
+using WebApplication_01.Utility;
 
 namespace WebApplication_01.Controllers
 {
     public class KitapTuruController : Controller
     {
+        private readonly UygulamaDbContext _uygulamaDbContext;
+
+        public KitapTuruController(UygulamaDbContext context)
+        {
+            _uygulamaDbContext = context;   
+        }   
         public IActionResult Index()
         {
-            return View();
+            List<KitapTuru> objKitapTuruList = _uygulamaDbContext.KitapTurleri.ToList();
+            return View(objKitapTuruList);
         }
+
     }
 }
